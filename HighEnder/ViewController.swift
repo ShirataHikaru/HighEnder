0c4ac2bc1fb30bca95da394d95b8857105adf337
ShirataHikaru <dendenden00@gmail.com>
2016-11-19 13:27:06 +0900

    Initial Commit

---
+++ HighEnder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HighEnder
+//
+//  Created by 白田光 on 2016/11/19.
+//  Copyright © 2016年 白田光. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
